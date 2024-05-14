f1b00fa73da0a1f56ec7b98d72bebbc2caf16aba
Aqila <aqela.af@gmail.com>
2024-05-14 11:16:46 +0200

    workflows fetched

--- cache/__init__.py
+++ cache/__init__.py
@@ -10,5 +10,10 @@ def save_data_to_csv(data: dict[str:str], file_name: str):
     file_name = PATH / file_name
     if os.path.isfile(file_name):
         return None
-    df = pd.DataFrame(data)
-    df.to_csv(file_name)
+    # Force every value to be a string
+    data = {k: str(v) for k, v in data.items()}
+    column_names = list(data.keys())
+    values = list(data.values())
+    df = pd.DataFrame([values], columns=column_names)
+    print(f"Saving data to {file_name}")
+    df.to_csv(file_name, index=False)
