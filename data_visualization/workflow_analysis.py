30ac299a745a2fc3ef792e8d06eb3b4bc7c8b792
Aqila <aqela.af@gmail.com>
2024-10-20 14:24:03 +0200

    modify the data preparation for creating plots

--- data_visualization/workflow_analysis.py
+++ data_visualization/workflow_analysis.py
@@ -3,39 +3,40 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the data from the CSV file
-data = pd.read_csv('traffic_control_final.csv')
+data = pd.read_csv('dataset/traffic_control_final.csv')
 
-# Filter data for run failures
-failures = data[data['run_conclusion'] == 'failure'].copy()
+# Filter data for workflow run triggered by pull request
+commit = data[data['event'] == 'push'].copy()
 
-# Determine the event type and corresponding time for each failure
-failures['time'] = failures.apply(lambda row: row['pull_request_created_at'] if row['event'] == 'pull_request' else row['commit_date'], axis=1)
+# Determine the event type and corresponding time for each run
+commit['time'] = commit.apply(lambda row: row['commit_date'], axis=1)
 
-# Aggregate the data based on class, affected files, workflow name, and run failure reasons
-failures_by_class = failures['class'].value_counts()
-affected_files = failures.groupby('files_changed').size().reset_index(name='counts')
+# Aggregate the data based on class, affected files, workflow name
+commit_label = commit['class'].value_counts()
+
+affected_files = commit.groupby('files_changed').size().reset_index(name='counts')
 
 # Convert the time column to datetime for timeline plotting
-failures['time'] = pd.to_datetime(failures['time'])
+commit['time'] = pd.to_datetime(commit['time'])
 
 # Save the prepared data for analysis into a new CSV file
-prepared_data_path = 'workflows.csv'
-failures.to_csv(prepared_data_path, index=False)
+prepared_data_path = 'data/prepared_data1.csv'
+commit.to_csv(prepared_data_path, index=False)
 
 
-# Pie chart for the distribution of run failures by class (TD vs. Not_TD)
+# Pie chart for the TD distribution within workflow runs triggered by commit
 plt.figure(figsize=(8, 8))
-plt.pie(failures_by_class, labels=failures_by_class.index, autopct='%1.1f%%', startangle=140)
-plt.title('Distribution of Run Failures by Class')
-plt.savefig('distribution.pdf')
+plt.pie(commit_label, labels=commit_label.index, autopct='%1.1f%%', startangle=140)
+plt.title('Distribution of TD within Workflow run that are triggered by commits')
+plt.savefig('pie_chart.png')
 plt.show()
 
 
-# Timeline chart for the time distribution of run failures, segmented by class (TD or Not_TD)
+# Plot for the TD distribution over time, segmented by class (TD or Not_TD)
 plt.figure(figsize=(14, 7))
-sns.scatterplot(x='time', y='run_number', hue='class', style='workflow_name', data=failures)
-plt.title('Time Distribution of Run Failures by Class (TD or Not_TD) and Workflow Name')
+sns.scatterplot(x='time', y='run_number', hue='class', style='workflow_name', data=commit)
+plt.title('TD distribution within workflows over time')
 plt.xlabel('Time')
-plt.ylabel('Run Number')
-plt.savefig('tdDistribution.pdf')
+plt.ylabel('Workflow_run Number')
+plt.savefig('td_distribution_overTime.png')
 plt.show()
